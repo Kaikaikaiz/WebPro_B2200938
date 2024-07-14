f75c56df8230d44ed25bbfb614a395b080c0bc6c
ANyangpa <171238641+ANyangpa@users.noreply.github.com>
2024-07-15 03:45:58 +0800

    Update MyAccountSignUp.php

--- Login System/MyAccountSignUp.php
+++ Login System/MyAccountSignUp.php
@@ -89,7 +89,7 @@ $isAdmin = $loggedIn && $_SESSION['username'] == 'admin';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign Up</title>
-    <link rel="stylesheet" href="../My Account/MyAccountSignUp.css"> <!-- Link your CSS file -->
+    <link rel="stylesheet" href="../Login System/MyAccountSignUp.css"> <!-- Link your CSS file -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
 </head>
